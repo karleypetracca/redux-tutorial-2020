250021103abf8c390425180b18d864d3a3a8e951
Karley Petracca <karleypetracca@gmail.com>
2020-04-06 13:41:25 -0400

    Feat: set up todos and visibility filters via redux

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,21 +1,21 @@
 import React from "react";
 import cx from "classnames";
 
-const Todo = ({ todo }) => (
-  <li
-    className="todo-item"
-    onClick={() => {} /** dispatches action to toggle todo */}
-  >
-    {todo && todo.completed ? "👌" : "👋"}{" "}
-    <span
-      className={cx(
-        "todo-item__text",
-        todo && todo.completed && "todo-item__text--completed"
-      )}
-    >
-      {todo.content}
-    </span>
-  </li>
+import { connect } from "react-redux";
+import { toggleTodo } from "../redux/actions";
+
+const Todo = ({ todo, toggleTodo }) => (
+	<li className="todo-item" onClick={() => toggleTodo(todo.id)}>
+		{todo && todo.completed ? "👌" : "👋"}{" "}
+		<span
+			className={cx(
+				"todo-item__text",
+				todo && todo.completed && "todo-item__text--completed"
+			)}
+		>
+			{todo.content}
+		</span>
+	</li>
 );
 
-export default Todo;
+export default connect(null, { toggleTodo })(Todo);
